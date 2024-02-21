629e977a95533a31edde7e25c3d3881fc00b7589
hanaru <133945249+1roo@users.noreply.github.com>
2024-02-21 18:26:04 +0900

    17. 자연수 뒤집어 배열로 만들기

---
+++ 17.js
@@ -0,0 +1,8 @@
+function solution(n) {
+    let nums = String(n).split('');
+    let answer = [];
+    for(let i = 0; i < nums.length; i++) {
+        answer[i] = parseInt(nums[nums.length-1-i]);
+    }
+    return answer;
+}
